c557b1fbdeb8bf92267c6e498247b820dcbbdc93
Sumyork <psychohistorian.p@gmail.com>
2024-12-30 01:15:04 +0300

    Внедрение коллекций | Поставщики контекста [#505300] - practice

--- src/main/java/ru/job4j/bmb/services/RecommendationEngine.java
+++ src/main/java/ru/job4j/bmb/services/RecommendationEngine.java
@@ -1,24 +1,23 @@
 package ru.job4j.bmb.services;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.BeanNameAware;
+import ru.job4j.bmb.content.Content;
+import ru.job4j.bmb.content.ContentProvider;
+
+import java.util.List;
+import java.util.Random;
 
 @Service
-public class RecommendationEngine implements BeanNameAware {
-    @Override
-    public void setBeanName(String name) {
-        System.out.println("RecommendationEngine class name: " + name);
+public class RecommendationEngine {
+    private final List<ContentProvider> contents;
+    private static final Random RND = new Random(System.currentTimeMillis());
+
+    public RecommendationEngine(List<ContentProvider> contents) {
+        this.contents = contents;
     }
 
-    @PostConstruct
-    public void init() {
-        System.out.println("RecommendEngine bean is going through init.");
-    }
-
-    @PreDestroy
-    public void destroy() {
-        System.out.println("RecommendEngine bean will be destroyed now.");
+    public Content recommendFor(Long chatId, Long moodId) {
+        var index = RND.nextInt(0, contents.size());
+        return contents.get(index).byMood(chatId, moodId);
     }
 }
